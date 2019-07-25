bebd0aa0667a814c9b20de35c000c9d2516c2dd2
RavinderGill <RavinderGill@9.199.51.97>
2019-07-25 13:56:01 +0530

    Second Commit

---
+++ SeleniumWebDriver/src/WebDriver/Demo9.java
@@ -0,0 +1,26 @@
+package WebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo9 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.toolsqa.com/automation-practice-switch-windows/");
+		driver.manage().window().maximize();
+		String parentWindow = driver.getWindowHandle();
+		System.out.println("Parent Window Name : " + parentWindow);
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("button#button1")).click();
+		
+		
+
+
+
+	}
+
+}
