9f7bcca84842c887da540034cbdd50757bc1659a
RavinderGill <RavinderGill@DESKTOP-92RCTKU.in.ibm.com>
2019-07-29 14:43:37 +0530

    Second Commit

--- SeleniumWebDriver/src/WebDriver/Demo10.java
+++ SeleniumWebDriver/src/WebDriver/Demo10.java
@@ -1,12 +1,16 @@
 package WebDriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Demo10 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -15,12 +19,33 @@ public class Demo10 {
 		
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		String title = js.executeScript("return document.title").toString();
-		System.out.println(title); 
-	
+		System.out.println(title);
 		
+		driver.navigate().refresh();
+		js.executeScript("history.go(0)");
 		
+		js.executeScript("document.getElementById('txtUsername').value='Admin'");
+		js.executeScript("document.getElementById('txtPassword').value='admin123'");
 		
 
+		WebElement ele = (WebElement)js.executeScript("return document.getElementById('txtUsername')");
+		String name = ele.getAttribute("value");
+		System.out.println(name);
+		Thread.sleep(2000);
+		js.executeScript("document.getElementById('btnLogin').click()");
+		
+		Thread.sleep(2000);
+		js.executeScript("document.getElementById('menu_pim_viewPimModule').click()");
+		
+		/*Thread.sleep(2000);
+		js.executeScript("document.getElementById('menu_recruitment_viewRecruitmentModule').click()");*/
+		
+		WebElement WE1 = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+		
+		Actions act = new Actions(driver);
+		
+		act.contextClick(WE1).sendKeys(Keys.ARROW_DOWN, Keys.RETURN).perform();
+		
 	}
 
 }
---
+++ SeleniumWebDriver/src/WebDriver/Demo8.java
@@ -0,0 +1,43 @@
+package WebDriver;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Demo8 {
+
+	public static void main(String[] args) throws AWTException {
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		
+		WebElement Gmail = driver.findElement(By.className("gb_e"));
+		Actions act = new Actions(driver);
+		
+		act.moveToElement(Gmail).keyDown(Keys.CONTROL).click().build().perform();
+		String title1 = driver.getTitle();
+		System.out.println(title1);
+		
+		/*Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_TAB);
+		
+		String title2 = driver.getTitle();
+		System.out.println(title2);*/
+		
+		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		System.out.println(tabs.size());
+		driver.switchTo().window(tabs.get(1));
+		
+		
+	}
+}
