24f32aeaa00d2d54b234e98181fc7a704db22e67
RavinderGill <RavinderGill@DESKTOP-92RCTKU>
2019-07-30 14:12:03 +0530

    Update

--- SeleniumWebDriver/src/WebDriver/Demo10.java
+++ SeleniumWebDriver/src/WebDriver/Demo10.java
@@ -1,12 +1,11 @@
 package WebDriver;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class Demo10 {
 
@@ -17,13 +16,15 @@ public class Demo10 {
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
 		
+		// Get Page Title using Java Script Executor
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		String title = js.executeScript("return document.title").toString();
 		System.out.println(title);
 		
+		// Refresh page using Java Script Executor
 		driver.navigate().refresh();
 		js.executeScript("history.go(0)");
-		
+		// Perform some actions using Java Script Executor
 		js.executeScript("document.getElementById('txtUsername').value='Admin'");
 		js.executeScript("document.getElementById('txtPassword').value='admin123'");
 		
@@ -37,15 +38,51 @@ public class Demo10 {
 		Thread.sleep(2000);
 		js.executeScript("document.getElementById('menu_pim_viewPimModule').click()");
 		
-		/*Thread.sleep(2000);
-		js.executeScript("document.getElementById('menu_recruitment_viewRecruitmentModule').click()");*/
+		//Thread.sleep(2000);
+		//js.executeScript("document.getElementById('menu_recruitment_viewRecruitmentModule').click()");
 		
-		WebElement WE1 = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+		// Scroll down using Coordinates
+		//js.executeScript("window.scrollBy(0,600)");
+		// Scroll Up using Coordinates
+		//js.executeScript("window.scrollBy(0,-600)");
+		// Scroll Back to default using Coordinates
+		//js.executeScript("window.scrollTo(0,0)");
 		
-		Actions act = new Actions(driver);
+		// Scroll till the end of the page
+		//js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+		//Thread.sleep(2000);
+		// Scroll up to the top of the page
+		//js.executeScript("window.scrollBy(0, -document.body.scrollHeight)");
 		
-		act.contextClick(WE1).sendKeys(Keys.ARROW_DOWN, Keys.RETURN).perform();
+		//js.executeScript("window.screenTop");
+		
+		//js.executeScript("document.getElementById('ohrmList_chkSelectRecord_5').scrollIntoView");
+		
+		System.out.println("---------- Using WebDriver ----------");
+		System.out.println(driver.findElement(By.xpath("//table[@id='resultTable']")).getText());
+		
+		// Using Java Script
+		System.out.println("================== Using Java Script =====================");
+		String TableData = js.executeScript("return document.getElementById('resultTable').innerText").toString();
+		System.out.println(TableData);
+		
+		String HTMLData = js.executeScript("return document.getElementById('resultTable').innerHTML").toString();
+		System.out.println(HTMLData);
+		
+		// navigating using java script
+		js.executeScript("window.history.back()");
+		driver.navigate().forward();
+		js.executeScript("window.history.forward()");
+		
+		
+		
+		js.executeScript("alert('Welcome to the session on selenium')");
+		
+		Alert alert = driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.accept();
+		
+		
 		
 	}
-
 }
